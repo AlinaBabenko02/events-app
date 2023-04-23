9f5db04ce0979e06da2cd7f2036000f7b5c2ce82
AlinaBabenko02 <74904112+AlinaBabenko02@users.noreply.github.com>
2023-04-24 01:08:02 +0200

    feat: event modal validation test with previous test fix

--- src/tests/app.test.tsx
+++ src/tests/app.test.tsx
@@ -1,39 +1,76 @@
 import "@testing-library/jest-dom";
-import { render, screen, waitFor } from "@testing-library/react";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import App from "../app/app";
-import { EventTypeStampLabel } from "../app/components/table/event-type-stamp";
+import { RenderSchemaPlaces } from "../data/enums";
+import { eventSchema } from "../mocks/data";
 import { events } from "../mocks/handlers";
 
 const queryClient = new QueryClient();
 
 describe("Events table", () => {
   test("Table render with the correct data", async () => {
-    render(
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    );
-    events.forEach(async (event) => {
-      await waitFor(() => {
+    await act(async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      );
+    });
+    // data is loading
+    expect(screen.getByText("No data")).toBeInTheDocument();
+    await waitForElementToBeRemoved(() => screen.queryByText("No data"));
+    // data loading finished
+
+    await waitFor(() => {
+      events.forEach((event) => {
         expect(screen.getByText(event.title)).toBeInTheDocument();
-      });
-      await waitFor(() => {
-        expect(
-          screen.getByText(EventTypeStampLabel[event.type])
-        ).toBeInTheDocument();
-      });
-      await waitFor(() => {
-        expect(
-          screen.getByText(event.startDate.toString())
-        ).toBeInTheDocument();
-      });
-      await waitFor(() => {
-        expect(screen.getByText(event.endDate.toString())).toBeInTheDocument();
-      });
-      await waitFor(() => {
         expect(screen.getByText(event.description)).toBeInTheDocument();
       });
     });
+  });
+});
+
+describe("Event modal", () => {
+  test("Create event modal validation", async () => {
+    await act(async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      );
+    });
+    const openModalButton = screen.getByText("Create event");
+    fireEvent.click(openModalButton);
+
+    // data loading
+    await waitFor(() => {
+      eventSchema
+        .filter((field) => field.render.includes(RenderSchemaPlaces.FORM))
+        .forEach((field) => {
+          expect(screen.getByText(field.label)).toBeInTheDocument();
+        });
+    });
+    // data loading finished
+
+    const createButton = screen.getByText("Create");
+    fireEvent.submit(createButton);
+
+    await waitFor(() => {
+      const messagesCount = screen.getAllByText("Required");
+      expect(messagesCount).toHaveLength(3);
+    });
+    expect(
+      screen.getByText(
+        "There are errors in the form. Please correct before saving."
+      )
+    ).toBeInTheDocument();
   });
 });
