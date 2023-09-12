b32e807f3c5a4f41c092a435697b6cc1c884e9d2
AlinaBabenko02 <your.alinaa@gmail.com>
2023-09-12 14:28:57 +0200

    fix: delete tests

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  setupFilesAfterEnv: ["./jest.setup.js"],
-  preset: "@vue/cli-plugin-unit-jest",
-  transformIgnorePatterns: ["node_modules/(?!axios)"],
-};
--- src/tests/typescript-extra.ts
+++
@@ -1,3 +0,0 @@
-//generic that takes an Array and return first element’s type
-
-type First<T extends any[]> = T extends [] ? never : T[0];
